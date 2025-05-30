292eb239e65e680bffd10c3338594a54b4d01a7f
Jonathan Lurie <lurie.jo@gmail.com>
2025-05-30 09:55:42 +0200

    first

---
+++ generate.sh
@@ -0,0 +1,4 @@
+spreet --retina ./src/diecut/svg ./sprite/phosphor-diecut@2x
+spreet ./src/diecut/svg ./sprite/phosphor-diecut
+spreet --retina ./src/square/svg ./sprite/phosphor-square@2x
+spreet ./src/square/svg ./sprite/phosphor-square
